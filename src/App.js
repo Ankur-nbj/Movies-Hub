2412108b30066bbb7cd58d02e1f6b3fff83afa63
Ankur Gangwar <ankurgangwar.nbj@gmail.com>
2023-09-07 17:01:18 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Component/Header/Header";
+import SimpleBottomNavigation from "./Component/MainNav";
+import { Container } from '@mui/material';
+import Trending from './Pages/Trending/Trending';
+import Movies from './Pages/Movies/Movies';
+import Search from './Pages/Search/Search';
+import Series from './Pages/Series/Series';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Header />
+    <div className="app">
+      <Container>
+        <Routes>
+          <Route path='/' component={Trending} exact/>
+          <Route path='/movies' component={Movies}/>
+          <Route path='/series' component={Series}/>
+          <Route path='/search' component={Search}/>
+        </Routes>
+      </Container>
+     </div>
+    <SimpleBottomNavigation />
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/Search/Search.js
@@ -0,0 +1,10 @@
+
+const Search = () => {
+  return (
+    <div>
+     <span classname="pageTitle">Search</span>
+    </div>
+  )
+}
+
+export default Search
